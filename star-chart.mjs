24332e2f21fdf24b2ed20fd36b0365031f58e96b
BrentNewhall <brent@brentnewhall.com>
2023-06-15 20:47:32 -0400

    Refactor stars into objects

--- star-chart.mjs
+++ star-chart.mjs
@@ -12,9 +12,9 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('canvas').appendChild(renderer.domElement);
 
-function createStar(scene, stars, x, y, z) {
+function createStar(scene, starData, x, y, z) {
     // Create a sphere geometry
-    const sphereGeometry = new THREE.SphereGeometry(0.1, 32, 32);
+    const sphereGeometry = new THREE.SphereGeometry(0.2, 32, 32);
 
     // Create a material for the core
     const sphereCoreMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
@@ -31,12 +31,13 @@ function createStar(scene, stars, x, y, z) {
 
     // Scale the glow sphere to make it larger than the core sphere
     starGlow.scale.multiplyScalar(1.9);
-    starCore.add(starGlow);
+    //starCore.add(starGlow);
 
     // Position the sphere in front of the camera
     starCore.position.set(x, y, z);
     scene.add(starCore); // Add the sphere as a child of the camera
-    stars.push(starCore);
+    //stars.push(starCore);
+    starData["object"] = starCore;
 }
 
 function createLine(scene, currPosition, prevPosition) {
@@ -52,63 +53,177 @@ function createLine(scene, currPosition, prevPosition) {
 scene.add(camera);
 
 const distance = 50; // Max distance of stars from galactic center
-const constellations = [
-    [
-        [0, 0, 1],
-        [3, 4, 0],
-        [8, 7, 0],
-        [15, 10, 3]
-    ],
-    [
-        [-1.2, 1.2, 2.2],
-        [-2.5, 7.83, 8.31],
-        [-8.73, 15.5, 9.98],
-        [-10.2, 18.5, 14.5]
-    ],
-    [
-        [6.2, 7.2, 2.95],
-        [7.2, 12.2, 4.1],
-        [10.38, 14.1, 12.95],
+const allStars = [
+    {
+        "constellation": "Andromeda",
+        "name": "A",
+        "position": [0,0,1]
+    },
+    {
+        "constellation": "Andromeda",
+        "name": "B",
+        "position": [3,4,0],
+        "resources": ["Artifacts"]
+    },
+    {
+        "constellation": "Andromeda",
+        "name": "C",
+        "position": [8,7,0]
+    },
+    {
+        "constellation": "Andromeda",
+        "name": "D",
+        "position": [15,10,3]
+    },
+    {
+        "constellation": "Rigel",
+        "name": "A",
+        "position": [-1.2, 1.2, 2.2]
+    },
+    {
+        "constellation": "Rigel",
+        "name": "B",
+        "position": [-2.5, 7.83, 8.31]
+    },
+    {
+        "constellation": "Rigel",
+        "name": "C",
+        "position": [-8.73, 15.5, 9.98]
+    },
+    {
+        "constellation": "Rigel",
+        "name": "D",
+        "position": [-10.2, 18.5, 14.5]
+    },
+    {
+        "constellation": "Betelgeuse",
+        "name": "A",
+        "position": [6.2, 7.2, 2.95]
+    },
+    {
+        "constellation": "Betelgeuse",
+        "name": "B",
+        "position": [7.2, 12.2, 4.1]
+    },
+    {
+        "constellation": "Betelgeuse",
+        "name": "C",
+        "position": [10.38, 14.1, 12.95]
+    },
+    {
+        "position": [23.33,-22.4,22.53]
+    },
+    {"position": [15.66,17.27,-5.08]},
+    {"position": [-14.53,-17.94,10.5]},
+    {"position": [-15.87,0.52,16.72]},
+    {"position": [-4.88,2.71,-6.79]},
+    {"position": [-12.65,13.06,-21.53]},
+    {"position": [12.95,-3.9,-9.51]},
+    {"position": [8.16,-24.77,4.78]},
+    {"position": [8.38,-17.9,-5.52]},
+    {"position": [11.59,-22.64,23.47]},
+    {"position": [-13.33,13.66,3.59]},
+    {"position": [21.48,-19.06,-1.38]},
+    {"position": [7.72,-16.49,9.37]},
+    {"position": [8.5,-23.7,16.78]},
+    {"position": [-21.06,-2.1,16.65]},
+    {"position": [-20.25,21.02,-13.99]},
+    {"position": [12.63,3.21,-15.97]},
+    {"position": [18.77,-10.54,-19.85]},
+    {"position": [-21.2,5.73,2.1]},
+    {"position": [0.8,-19.94,12.61]},
+    {"position": [12.8,18.1,-21.3]},
+    {"position": [18.1,14.92,-23.7]},
+    {"position": [16.1,-1.3,8.9]},
+    {"position": [-3.88,-20.07,9.73]},
+    {"position": [21.67,-11.79,14.38]},
+    {"position": [-16.22,23.45,10.56]},
+    {"position": [-11.52,-14.91,18.41]},
+    {"position": [-7.33,17.44,15.56]},
+    {"position": [5.35,11.74,13.76]},
+    {"position": [21.95,-2.26,-14.31]},
+    {"position": [-4.94,-10.81,-3.63]},
+    {"position": [-1.52,23.35,-14.06]},
+    {"position": [-8.06,-17.29,4.94]},
+    {"position": [-19.74,-1.57,-8.28]},
+    {"position": [4.72,-21.44,-1.16]},
+    {"position": [-0.86,18.9,23.6]},
+    {"position": [-3.34,-19.51,-12.6]},
+    {"position": [6.54,-6.3,8.97]}
 ]
-];
-const otherStars = [
-    [23.33,-22.4,22.53],
-    [15.66,17.27,-5.08],
-    [-14.53,-17.94,10.5],
-    [-15.87,0.52,16.72],
-    [-4.88,2.71,-6.79],
-    [-12.65,13.06,-21.53],
-    [12.95,-3.9,-9.51],
-    [8.16,-24.77,4.78],
-    [8.38,-17.9,-5.52],[11.59,-22.64,23.47],[-13.33,13.66,3.59],[21.48,-19.06,-1.38],[7.72,-16.49,9.37],
-    [8.5,-23.7,16.78],[-21.06,-2.1,16.65],[-20.25,21.02,-13.99],[12.63,3.21,-15.97],[18.77,-10.54,-19.85],
-    [-21.2,5.73,2.1],[0.8,-19.94,12.61],[12.8,18.1,-21.3],[18.1,14.92,-23.7],[16.1,-1.3,8.9],
-    [-3.88,-20.07,9.73],[21.67,-11.79,14.38],[-16.22,23.45,10.56],[-11.52,-14.91,18.41],[-7.33,17.44,15.56],
-    [5.35,11.74,13.76],[21.95,-2.26,-14.31],[-4.94,-10.81,-3.63],[-1.52,23.35,-14.06],[-8.06,-17.29,4.94],
-    [-19.74,-1.57,-8.28],[4.72,-21.44,-1.16],[-0.86,18.9,23.6],[-3.34,-19.51,-12.6],[6.54,-6.3,8.97]
-]
+
 const stars = [];
-for( let j = 0; j < constellations.length; j++ ) {
-    const positions = constellations[j];
-    for (let i = 0; i < positions.length; i++) {
-        createStar(scene, stars, positions[i][0], positions[i][1], positions[i][2]);
-        if( i > 0 ) {
-            createLine(scene, positions[i], positions[i-1]);
+
+let currConstellation = null;
+for( const [index,starData] of allStars.entries() ) {
+    if( "constellation" in starData ) {
+        if( currConstellation == starData["constellation"] ) {
+            // Draw line
+            createLine(scene, allStars[index].position, allStars[index-1].position);
         }
+        currConstellation = starData["constellation"];
     }
-}
-for (let i = 0; i < otherStars.length; i++) {
-    createStar(scene, stars, otherStars[i][0], otherStars[i][1], otherStars[i][2]);
+    // Draw star
+    createStar(scene, starData, starData.position[0], starData.position[1], starData.position[2]);
 }
 
 // Animation loop
 function animate() {
     requestAnimationFrame(animate);
+    rotateScene(0.1);
     renderer.render(scene, camera);
 }
 
 let isDragging = false;
 let previousMouseX = 0;
+
+function changeStar(star,color,radius) {
+    star.material.color = new THREE.Color(color);
+    star.geometry.radius = radius;
+    star.geometry.computeBoundingSphere();
+    const currentRadius = star.geometry.boundingSphere.radius;
+    const scale = radius / currentRadius;
+    star.scale.set(scale, scale, scale);
+}
+
+function changeStarDetails(star, active) {
+    const constellation = star["constellation"].toLowerCase();
+    const name = star["name"].toLowerCase();
+    const starID = constellation + "-" + name;
+    (active === "active") ? document.getElementById(constellation).classList.add("active") : document.getElementById(constellation).classList.remove("active"); 
+    (active === "active") ? document.getElementById(starID).classList.add("active") : document.getElementById(starID).classList.remove("active");
+    let details = "<h2>" + star["constellation"] + " " + star["name"] + "</h2>";
+    if( "resources" in star ) {
+        details += "<p><strong>Resources:</strong> ";
+        for( const [index,resource] of star["resources"].entries() ) {
+            details += resource + ",";
+        }
+        if( star["resources"].length > 0 ) {
+            details = details.slice(0, -1);
+        }
+        details += "</p>";
+    }   
+    document.getElementById("details").innerHTML = details;
+    const btn = document.createElement("button");
+    btn.innerText = "Explore";
+    document.getElementById("details").appendChild(btn);
+}
+
+let currStar = -1;
+document.addEventListener('keyup', (event) => {
+    if (event.key === ' ') {
+        let star = null;
+        if( currStar >= 0 ) {
+            star = allStars[currStar]["object"];
+            changeStar( star, 0xffffff, 0.2);
+            changeStarDetails(allStars[currStar], "inactive");
+        }
+        currStar = (currStar + 1) % 11;
+        star = allStars[currStar]["object"];
+        changeStar( star, 0xffee66, 0.5);
+        changeStarDetails(allStars[currStar], "active");
+    }
+});
 
 document.addEventListener('mousedown', (event) => {
     isDragging = true;
@@ -129,11 +244,9 @@ document.addEventListener('mouseup', (event) => {
 });
 
 function checkClicked(event) {
-    console.log("clicked")
     const mouse = new THREE.Vector2();
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-    console.log(mouse)
   
     // Create a raycaster
     const raycaster = new THREE.Raycaster();
@@ -145,7 +258,6 @@ function checkClicked(event) {
   
     // If there is an intersection, call your desired function
     if (intersects.length > 0) {
-        console.log("found")
         const closestObject = findClosestObject(intersects, raycaster.ray.origin);
         handleClosestObject(closestObject);
     }
@@ -168,7 +280,6 @@ function findClosestObject(intersects, origin) {
   
 function handleClosestObject(object) {
     // Perform your desired action with the closest object
-    console.log('Closest object:', object);
 }
 
 function rotateScene(delta) {
